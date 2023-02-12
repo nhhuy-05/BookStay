cec0ca59f5a47332a0fe92d90c2b8ddd948b9426
Huy Nguyen <huuhuy5208@gmail.com>
2023-02-12 10:53:29 +0700

    Main Activity
    - Add Bottom Bar
    - Add Fragment for Main Activity: Home, Saved, Bookings, Profile

--- app/src/main/java/vn/edu/fpt/projectprm392/activities/SearchResultActivity.java
+++ app/src/main/java/vn/edu/fpt/projectprm392/activities/SearchResultActivity.java
@@ -1,15 +1,16 @@
 package vn.edu.fpt.projectprm392.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import vn.edu.fpt.projectprm392.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class SearchResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search_result);
     }
 }
